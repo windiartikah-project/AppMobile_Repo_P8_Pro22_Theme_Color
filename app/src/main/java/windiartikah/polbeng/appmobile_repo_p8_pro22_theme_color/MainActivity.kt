b7c8eb8dd985da677339416df8d3910c21712ad1
RPL-22 <annisafitriannisa1@gmail.com>
2022-06-17 10:47:21 +0700

    theme color

---
+++ app/src/main/java/windiartikah/polbeng/appmobile_repo_p8_pro22_theme_color/MainActivity.kt
@@ -0,0 +1,11 @@
+package windiartikah.polbeng.appmobile_repo_p8_pro22_theme_color
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
